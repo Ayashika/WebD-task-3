f345b0555ea51e1bb2e4e03bcc63e368210a89c5
Ayashika <ayashika2624@gmail.com>
2022-01-24 20:02:48 +0530

    task 3 completed

---
+++ index3.js
@@ -0,0 +1,53 @@
+showNotes();
+
+let addbutton= document.getElementById("addbutton");
+addbutton.addEventListener("click", function(e) {
+  let addtext = document.getElementById("addtext");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addtext.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addtext.value = "";
+  showNotes();
+});
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary"><i class="far fa-trash-alt"></i></button>
+                        </div>
+                </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Please write your note in above section.`;
+  }
+}
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
